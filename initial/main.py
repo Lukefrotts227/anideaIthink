dbf8f507624cc18bb43e23b5b3498c94777d22c3
Lukas Frotton <132273525+Lukefrotts227@users.noreply.github.com>
2023-12-14 02:08:57 +0000

    starting to learn evo algs

--- initial/main.py
+++ initial/main.py
@@ -1,6 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+class baseSuperBot: 
+    def __init__(self): 
+        pass
+    
 class baseBot: 
     def __init__(self, goal): 
         pass
+
+
+'''
+superbot in charge of other bots
+all the bots work together to achieve a common goal 
+each bot has a different task in said goal
+evoulutionary algorithm to have bots create better versions of themselves
+need a function the have the bots evaluate themselves while using the context of the other different bots
+'''
